956177d5d3f7cc11ecb94b6a3ef75eef24872990
Enrique Garcia <egaillera@gmail.com>
2025-08-12 11:39:58 +0200

    [feat] first version of tests

---
+++ SimpleMusicTests/SimpleMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMusicTests.swift
+//  SimpleMusicTests
+//
+//  Created by Enrique Garcia Illera on 12/8/25.
+//
+
+import Testing
+@testable import SimpleMusic
+
+struct SimpleMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
